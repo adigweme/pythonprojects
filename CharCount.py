9783b9c5ab2b5f55c0dfd85b83a6c59bc028bf3c
Patrick Adigweme <patrick.adigweme@gmail.com>
2022-10-18 16:41:13 -0700

    update CharCount.py
    word count was including special characters that had a space on either side. ex. - 
     updated to fix word count code to exclude special characters.

--- CharCount.py
+++ CharCount.py
@@ -2,33 +2,45 @@
 import sys
 
 # establish variables to be used within script
-add = 0
+without_space = 0
 with_space = 0
 result = []
+parsed_specials = 0
+# identify special characters
+specials = [ ",", "-", ".", "?", "=", "!", ";", "+", ":"]
+print("Welcome to Character Counter.")
+print("Please input your text.")
+print(">>>")
 init = input()
 
 # main if statement to handle scanning user inputted text
 if len(init)>1:
    n = len(init)
-   for n in range(1, n):
+   for n in range(0, n):
 # count characters and words in this next block
-        without_space +=int(len(init[n]))
-        with_space = add + init.count(' ')
-        words = init.count(' ') + 1
+        with_space +=int(len(init[n]))
+        without_space = with_space - init.count(' ')
 # output results of character count and word count.
    print("Total character count, without spaces, is", without_space)
    print("Total character count, with spaces, is", with_space)
-   print("Total word count is", words)
 # turn user input into a string
    string =  str(init)
-# identify special characters
-   specials = [ ",", "-", ".", "?", "=", "-", ";", "+", ":"]
 # for loop to iterate through special characters, searching user input
    for char in specials:
         if char in string:
             result += char
         else:
             continue
+# parse input to identify word count 
+   word_parse = init.split(' ')
+# ensure no special characters are included in word count
+   for char in specials:
+        if char in word_parse:
+            parsed_specials = parsed_specials + word_parse.count(char)
+        else:
+            continue
+   words = int(len(word_parse)) - int(parsed_specials)
+   print("Total word count is", words)
 # if statement to allow user to choose whether to print special characters
    if len(result) > 0:
         print("Found", len(result), "special characters")

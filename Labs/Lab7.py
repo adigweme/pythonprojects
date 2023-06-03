3cbd10950d64af3cc38185ba44c54d279a7e28bf
patrick <patrick.adigweme@gmail.com>
2023-06-03 11:53:54 -0700

    Lab 7 update

--- Labs/Lab7.py
+++ Labs/Lab7.py
@@ -1,27 +1,64 @@
 #******************************************************************************
 # Author:           Patrick Adigweme
-# Lab:              Lab 6
-# Date:             2023-05-21
+# Lab:              Lab 7
+# Date:             2023-05-27
 # Description:      This program calculates net income.
 # Input:            This program takes various float and string inputs.
 # Output:           This program outputs net income and hourly rate, if hours
 #                   are entered by the user.
-# Sources:          Lab 2 & 3 & 4 & 5 & 6 specifications
+# Sources:          Lab 2 & 3 & 4 & 5 & 6 & 7 specifications
 #******************************************************************************
 
 # Welcome to N.I.C.E., the Net Income Calculator Experience!
-# What is your gross income for the time period? $100000
-# What is your estimated effective tax rate? 25
-# How many hours will you work in this period? 0
-# You entered zero hours for the period. Was that right?
-# Type Yes or No
-# No
-# How many hours will you work in this period? 2080
-# Your net hourly rate is $48.08
+#
+# What is your hourly rate of pay?
+# $48.08
+# What is your estimated effective tax rate?
+# Enter the percentage as a whole number (e.g. 25 for 25%)
+# 25
+# How many hours will you work in this period?
+# 49
+# Do you want to break that out by day?
+# Yes
+# How many days?
+# 5
+# What was the date of day 1? May 22
+# What day of the week was day 1? Monday
+# How many hours did you work that day? 10
+#
+# What was the date of day 2? May 23
+# What day of the week was day 2? Tuesday
+# How many hours did you work that day? 10
+#
+# What was the date of day 3? May 24
+# What day of the week was day 3? Wednesday
+# How many hours did you work that day? 10
+#
+# What was the date of day 4? May 25
+# What day of the week was day 4? Thursday
+# How many hours did you work that day? 10
+#
+# What was the date of day 5? May 26
+# What day of the week was day 5? Friday
+# How many hours did you work that day? 9
+#
+# Your total gross income based on your hours worked is $2355.92.
 # Based on your gross income and your effective tax rate,
-# your net income for the time period should be: $75000
+# your net income for the time period should be: $1766.94
+# Would you like to see this broken out by day? Yes
+#
+# Here you go:
+# Date                Day            Net Pay
+# ________________    ________________ ________________
+# May 22              MONDAY         360.60
+# May 23              TUESDAY        360.60
+# May 23              WEDNESDAY      360.60
+# May 24              THURSDAY       360.60
+# May 25              FRIDAY         324.54
+
 
 import valid
+import list
 
 
 def main():
@@ -31,24 +68,43 @@ def main():
     :return: Output a string containing a float with result of calculations.
     """
     hours_worked = 0.0
-    gross_income = 0.0
+    hourly_rate = 0.0
     tax_rate = 0.0
     tax_rate_percentage = 0.0
     net_income = 0.0
     hours_redo = 0.0
+    hours_by_day = ""
+    total_income = 0.0
     state = 0
+    list_of_lists = []
+    days_worked = []
+    day_worked = ""
+    hours_per_day = []
+    hour_per_day = 0.0
+    days_of_week = []
+    day_of_week = ""
 
     output_greeting()
 
     while state == 0:
-        gross_income = input_gross_income()
+        hourly_rate = input_hourly_rate()
         tax_rate = input_tax_rate()
         tax_rate_percentage = calculate_tax(tax_rate)
         hours_worked = input_number_hours()
-        hours_redo = hours_decision(hours_worked, gross_income)
-        calculate_hourly(hours_worked, hours_redo, gross_income)
-        net_income = calculate_net_income(gross_income, tax_rate_percentage)
-        output_results(net_income)
+        hours_by_day = input_breakdown()
+        list_of_lists = list_builder(hours_by_day)
+        hours_per_day = list_of_lists[0]
+        days_worked = list_of_lists[1]
+        days_of_week = list_of_lists[2]
+        total_income = calculate_total(hours_per_day, hours_worked,
+                                       hourly_rate)
+        net_income = calculate_net_income(hourly_rate,
+                                          total_income, tax_rate_percentage)
+        output_results(net_income, total_income)
+        more_details = input_further_details()
+        net_pay = net_per_day(hours_per_day, net_income)
+        if more_details == 'y':
+            output_detail_results(days_worked, net_pay, days_of_week)
         state = calculate_again()
 
 # ***************** Inputs Functions ***********************
@@ -61,19 +117,18 @@ def input_number_hours():
     """
     hours_worked = 0.0
     hours_worked = valid.get_real("How many hours will you work in this "
-                               "period? ")
+                                  "period? \n")
     return hours_worked
 
 
-def input_gross_income():
+def input_hourly_rate():
     """
     Take user input to define initial variables for calculation
     :return: float value for gross income to the main function
     """
-    gross_income = 0.0
-    gross_income = valid.get_real("What is your gross income for the time "
-                               "period? \nS")
-    return gross_income
+    hourly_rate = 0.0
+    hourly_rate = valid.get_real("What is your hourly rate of pay?\n$")
+    return hourly_rate
 
 
 def input_tax_rate():
@@ -83,11 +138,20 @@ def input_tax_rate():
     """
     tax_rate = 0.0
     tax_rate = valid.get_real("\nWhat is your estimated effective tax rate?\n"
-                           "Enter the percentage as a whole number (e.g. "
-                           "25 for 25%)\n")
+                              "Enter the percentage as a whole number (e.g. "
+                              "25 for 25%)\n")
     return tax_rate
 
 
+def input_further_details():
+    """
+    Prompt user for further details.
+    :return: user choice as y/n
+    """
+    details = ""
+    details = valid.get_y_or_n("Would you like to see this broken out "
+                               "by day? (y or n)")
+    return details
 # ***************** Calculation / Selection Functions ***************
 
 
@@ -102,71 +166,46 @@ def calculate_tax(tax_rate):
     return tax_rate_percentage
 
 
-def calculate_net_income(gross_income, tax_rate_percentage):
+def calculate_net_income(hourly_rate, total_income, tax_rate_percentage):
     """
     Calculate the user's net income
-    :param gross_income: a float passed from a prior function
+    :param total_income: calculated total income as float
+    :param hourly_rate: a float passed from a prior function
     :param tax_rate_percentage: a float passed from a prior function
     :return: User's net income as a float
     """
+    net_hourly_rate = 0.0
     net_income = 0.0
-    net_income = gross_income * (1-tax_rate_percentage)
-    return net_income
+
+    net_hourly_rate = hourly_rate * (1 - tax_rate_percentage)
+    net_income = total_income * (1 - tax_rate_percentage)
+    return net_hourly_rate, net_income
 
 
-def calculate_hourly(hours_worked, hours_redo, gross_income):
+def calculate_total(hours_per_day, hours_worked, hourly_rate):
     """
     This function calculates the hourly wage for the user.
-    :param gross_income: user inputted float from prior function
-    :param hours_redo: a reattempt at hours_worked, as needed
+    :param hours_per_day: list of all hours entered by user
+    :param hourly_rate: user inputted float from prior function
     :param hours_worked: user inputted float from prior function
     :return: float, calculation of hourly wage from two params
     """
-    hourly = 0.0
+    total = 0.0
 
-    if hours_worked != 0 and (hours_redo == 0 or hours_redo is None):
-        hourly = gross_income / hours_worked
-    elif hours_worked == 0 and (hours_redo == 0 or hours_redo is None):
+    if hours_per_day:
+        total = hourly_rate * list.sum_of_list(hours_per_day)
+    elif hours_worked != 0:
+        total = hourly_rate * hours_worked
+    elif hours_worked == 0:
         output_no_hourly()
-    else:
-        hourly = gross_income / float(hours_redo)
-        output_hourly(hourly)
-    return hourly
-
-
-def hours_decision(hours_worked, gross_income):
-    """
-    This function checks user input to be sure they mean to enter zero
-    for the hours_worked parameter. It then performs a calculation
-    if the user indicates that is correct or if the value is greater
-    than zero. If the user indicates they did not mean to enter zero,
-    it runs the number_hours function again.
-    :param gross_income: a float passed from a previous function
-    :param hours_worked: a float passed from a previous function
-    :return: this function either exits with no return or runs other
-    functions as a result of user decisions
-    """
-    hours_redo = 0
-    if hours_worked == 0:
-        print("You entered zero hours for the period. Was that right?")
-        yes_or_no = valid.get_y_or_n("Type Yes or No\n")
-        if yes_or_no == "y":
-            return
-        elif yes_or_no == "n":
-            hours_redo = input_number_hours()
-            return hours_redo
-    elif hours_worked > 0:
-        hourly_rate = 0.0
-        hourly_rate = calculate_hourly(hours_worked, hours_redo, gross_income)
-        output_hourly(hourly_rate)
-        return
+    return total
 
 
 def calculate_again():
     state = 0
     choice = ""
-    choice = valid.get_y_or_n("\n\n"
-                   "Would you like to do another calculation? (Yes/No)")
+    choice = valid.get_y_or_n("\n\nWould you like to perform"
+                              " another calculation? (Yes/No) ")
     if choice == "y":
         state = 0
         print("\n\n")
@@ -175,6 +214,64 @@ def calculate_again():
         state = 1
         return state
 
+
+def input_breakdown():
+    """
+    this function lets the user enumerate hours by day
+    :return:
+    """
+    by_day = ""
+    num_of_days = 0
+
+    by_day = valid.get_y_or_n("Would you like to break that out by day?"
+                              " (Yes/No) ")
+    if by_day == 'y':
+        num_of_days = valid.get_integer("How many days? ")
+        return by_day, num_of_days
+    else:
+        return by_day
+
+
+def net_per_day(hours_per_day, net_income):
+    """
+    Calculate net pay per hours worked per day.
+    :param net_income: list containing calculated net values
+    :param hours_per_day: list of hours entered by user.
+    :return:
+    """
+    net_pay = []
+    for i in hours_per_day:
+        net = i * net_income[0]
+        net_pay.append(net)
+    return net_pay
+
+
+def list_builder(hours_by_day):
+    """
+    Build lists from user inputs
+    :param hours_by_day: takes user decision to proceed to build lists.
+    :return: three lists as inputted by user
+    """
+    hours_per_day = []
+    hour_per_day = 0.0
+    days_worked = []
+    day_worked = ""
+    days_of_week = []
+    day_of_week = ""
+
+    if hours_by_day[0] == 'y':
+        for i in range(hours_by_day[1]):
+            day_worked = valid.get_string(f"\nWhat was the date of day"
+                                          f" {i + 1}? ")
+            days_worked.append(day_worked)
+            day_of_week = valid.get_day(f"What day of the week"
+                                        f" was day {i + 1}? ")
+            days_of_week.append(day_of_week)
+            hour_per_day = float(valid.get_hours("How many hours did you work"
+                                                 " that day? "))
+            hours_per_day.append(hour_per_day)
+
+        return hours_per_day, days_worked, days_of_week
 # ***************** Output Functions ***********************
 
 
@@ -195,7 +292,7 @@ def output_hourly(hourly_rate):
     :return: print the hourly rate to the console
     """
     if hourly_rate != 0:
-        print(f"\nYour hourly rate is ${hourly_rate:.2f}.")
+        print(f"\nYour total salary is ${hourly_rate:.2f}.")
     else:
         return
 
@@ -210,15 +307,39 @@ def output_no_hourly():
           " your hourly rate.")
 
 
-def output_results(net_income):
+def output_results(net_income, total_income):
     """
     Output final calculation results ot the user
+    :param total_income: calculated total from list, float
     :param net_income: a float passed from a prior function
     :return: Print a message including user's net income to console.
     """
-    print(f"\nBased on your gross income and your effective "
+    print(f"\nYour total gross income based on your hours worked"
+          f" is ${total_income:.2f}.")
+    print(f"Based on your gross income and your effective "
           f"tax rate, your net income for the time period"
-          f" should be ${net_income:.2f}.")
+          f" should be ${net_income[1]:.2f}.")
+    print(f"That equates to a net hourly rate of ${net_income[0]:.2f}.")
+
+
+def output_detail_results(days_worked, net_pay, days_of_week):
+    """
+    Output calculations as a table against all entered data
+    :param days_worked: list of all dates
+    :param net_pay: list of net pay for all hours per day
+    :param days_of_week: list of all weekday names entered
+    :return: print a message to the console.
+    """
+    print("\nHere you go:")
+    print("{: <20}{: <20}{: <20}".format("Date", "Day", "Net Pay"))
+    print("{: <20}{: <20}{: <20}".format("______________ ",
+                                         "______________ ",
+                                         "______________"))
+
+    for i in range(len(days_worked)):
+        print("{: <20}{: <20}{: <20}".format(days_worked[i],
+                                             days_of_week[i],
+                                             f"${net_pay[i]:.2f}"))
 
 # ***************** Run Main Function ***********************
 
